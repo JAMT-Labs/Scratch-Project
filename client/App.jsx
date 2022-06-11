fd80aca92ea6cb2424c8f5b893fffa15f5e58bbb
Justin Wouters <JustinWouters@gmail.com>
2022-06-11 09:06:48 -0700

    Boilerplate Complate

--- client/App.jsx
+++ client/App.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 function App() {
   // state
 
-  return <div className="App">hello world!</div>;
+  return <div className="App">boilerplate 90%</div>;
 }
 
 export default App;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const port = 3000;
+
+//Stating the server
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(express.static(path.resolve(__dirname, '../client')));
+
+// Statically serve everything in the build folder on the route
+app.use('/build', express.static(path.join(__dirname, '../build')));
+
+//We need to serve index.html on the route /
+app.get('/', (req, res) => {
+  //Two dots to get up one directory, to get to client (In server folder, move up one dir to scratch-project, move down to client, access index.html)
+  return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`listening on port: ${port}...`);
+});
