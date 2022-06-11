f22ae19fd98fe23a1aa8cc9026096fe2e02b7421
Miranda Jaramillo Morales <mirandajaramillo@MacBook-Pro-de-Miranda-2.local>
2022-06-11 14:25:57 -0500

    Added name Miranda

--- client/App.jsx
+++ client/App.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 function App() {
   // state
 
-  return <div className="App">boilerplate 90% Tarik Justin Alex</div>;
+  return <div className="App">boilerplate 90% Tarik Justin Alex Miranda  c: </div>;
 }
 
 export default App;
