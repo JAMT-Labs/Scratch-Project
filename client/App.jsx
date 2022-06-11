fc9e8158dee12b1ca32452da291ef1220b51b2ee
Alex Ryu <alexryu@Alexs-MacBook-Air-M1.local>
2022-06-11 11:43:07 -0700

    added name 'Alex'

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
   // state
 
-  return <div className="App">boilerplate 90% Tarik Justin</div>;
+  return <div className="App">boilerplate 90% Tarik Justin Alex</div>;
 }
 
 export default App;
