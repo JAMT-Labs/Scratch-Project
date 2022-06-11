36d3a56d1ce19af419e57d7aa73423da34e0afd4
Tarik Mokhtech <tarik.mokhtech@gmail.com>
2022-06-11 13:34:21 -0400

    Added name

--- client/App.jsx
+++ client/App.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 function App() {
   // state
 
-  return <div className="App">boilerplate 90%</div>;
+  return <div className="App">boilerplate 90% Tarik</div>;
 }
 
 export default App;
